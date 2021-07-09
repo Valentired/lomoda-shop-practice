eaa74a12cbb42ac6e587fe5f193cbcb158c54de0
Valentired <droopygreene@gmail.com>
2021-07-09 16:11:54 +0300

    добавил функционал открытия/закрытия окна корзины

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,23 @@ headerCityButton.addEventListener("click", () => {
     headerCityButton.textContent = city;
     localStorage.setItem('lomoda-location', city);
 });
+
+//модальное окно корзины
+const subheaderСart = document.querySelector(".subheader__cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+
+const cartModalOpen = () => cartOverlay.classList.add("cart-overlay-open");
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove("cart-overlay-open");
+}
+
+subheaderСart.addEventListener("click", cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.matches(".cart__btn-close") || target.matches(".cart-overlay")) {
+		cartModalClose();
+	}
+});
